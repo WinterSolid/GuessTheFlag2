a54a5c70d96d6d89c24b29e4a0c86f52a147c423
ZakTan <zakee.tanks@gmail.com>
2025-07-07 14:49:45 -0700

    refactor flag presentation logic, rename file contentView to GuessGameView, generate random flags w/ Answers. test question answer logic using a print statement.

---
+++ GuessTheFlag2/GuessGameView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  GuessTheFlag2
+//
+//  Created by Zakee Tanksley on 7/7/25.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct GuessGameView: View {
+    private let countries = ["United Kingdom","United States","Monaco","Ukraine","Estonia",
+                             "France","Germany","Ireland","Italy","Nigeria","Poland","Spain"]
+
+    @State private var options: [String] = []
+    @State private var correctAnswer = 0
+
+    var body: some View {
+        GeometryReader { geo in
+            let isLandscape = geo.size.width > geo.size.height
+
+            ZStack {
+                Color.secondary.ignoresSafeArea()
+
+                VStack(spacing: 20) {
+                    Text(options.isEmpty ? "???" : options[correctAnswer])
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .padding(.top, 15)
+
+                    Group {
+                        if isLandscape {
+                            HStack(spacing: 30) { flagButtons }
+                        } else {
+                            VStack(spacing: 30) { flagButtons }
+                        }
+                    }
+
+                    Text("Tap the Correct Flag")
+                        .font(.footnote)
+                        .foregroundStyle(.white)
+                        .padding(.bottom, 5)
+                }
+                .padding(.horizontal)
+            }
+            .onAppear(perform: generateFlags)
+        }
+    }
+
+    // Flag buttons
+    var flagButtons: some View {
+        ForEach(options.indices, id: \.self) { index in
+            Button {
+                if index == correctAnswer {
+                    print(" Correct: \(options[index])")
+                } else {
+                    print("Wrong: \(options[index])")
+                }
+                generateFlags()              // new set
+            } label: {
+                Image(options[index])
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .shadow(radius: 10)
+            }
+        }
+    }
+
+    // Generate unique flags
+    func generateFlags() {
+        options = Array(countries.shuffled().prefix(3))
+        correctAnswer = Int.random(in: 0..<options.count)
+    }
+}
+
+
+
+
+#Preview {
+  GuessGameView()
+}
+
