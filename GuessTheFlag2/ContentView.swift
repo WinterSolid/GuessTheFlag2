7564fde4b848b616b150415a0508c22a8b507d4c
ZakTan <zakee.tanks@gmail.com>
2025-07-07 12:00:33 -0700

    Added Flags as buttons, Text of Flag name as Correct answer and text "Tap the Correct Flag"

--- GuessTheFlag2/ContentView.swift
+++ GuessTheFlag2/ContentView.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-    var body: some View {
+  var countries = ["Estonia","France","Germany","Ireland", "Italy","Monaco","Nigeria", "Poland", "Spain", "UK","Ukraine","US"]
+  var correctAnswer = Int.random(in: 0...2)
+  var body: some View {
+    
+    ZStack {
+      Color.gray
+        .ignoresSafeArea()
+      VStack(spacing: 30) {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text(countries[correctAnswer])
+            .font(.headline)
+            .fontWeight(.bold)
+            .foregroundStyle(.white)
+            .padding()
+          
+          ForEach (0..<3) { number in
+            Button{
+              
+            } label: {
+              Image(countries[number])
+            }.padding()
+          }
+          
+          Text("Tap the Correct Flag")
+            .font(.footnote).foregroundStyle(.white)
+            .padding()
+          
+          
         }
-        .padding()
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
