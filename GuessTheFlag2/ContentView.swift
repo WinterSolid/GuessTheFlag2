30c5ba190a3ed27be05649d249f6f822c857dab6
ZakTan <zakee.tanks@gmail.com>
2025-07-07 10:37:09 -0700

    Initial Commit

---
+++ GuessTheFlag2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GuessTheFlag2
+//
+//  Created by Zakee Tanksley on 7/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag2/GuessTheFlag2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag2App.swift
+//  GuessTheFlag2
+//
+//  Created by Zakee Tanksley on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
